6d4996a02b38d0cf1dbc6b830fc0656cd6655ffb
hzambenedetti <hique.rz03@gmail.com>
2025-08-28 19:49:58 -0300

    feat#atividade-01: Implemented basic structure of auction-srv

--- atividade-01/auction_system/auction-srv/src/main.rs
+++ atividade-01/auction_system/auction-srv/src/main.rs
@@ -1,42 +1,86 @@
 use lapin::{
-    options::{BasicConsumeOptions, QueueDeclareOptions},
-    types::FieldTable,
-    Connection, ConnectionProperties,
+    options::{QueueDeclareOptions}, types::FieldTable, Connection, ConnectionProperties
 };
-use futures_lite::stream::StreamExt;
+use tokio::runtime::Runtime;
+use std::time::{SystemTime, UNIX_EPOCH};
+use crate::auction::Auction;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+pub mod auction;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Runtime::new()?.block_on(run())
+}
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "amqp://guest:guest@127.0.0.1:5672/%2f";
     let conn = Connection::connect(addr, ConnectionProperties::default()).await?;
-    println!("Consumer connected to RabbitMQ!");
 
     let channel = conn.create_channel().await?;
 
-    channel
-        .queue_declare(
-            "test_queue",
-            QueueDeclareOptions::default(),
-            FieldTable::default(),
+    let queues = [
+        "leilao_iniciado",
+        "leilao_finalizado"
+    ];
+
+    for &queue in queues.iter(){
+        channel.queue_declare(
+            queue, 
+            QueueDeclareOptions::default(), 
+            FieldTable::default()
         )
         .await?;
+    }
+    
+    let auctions = get_auctions();
+    let mut auctions_ref: Vec<(&Auction, u128)>  = Vec::with_capacity(2 * auctions.len());
+    auctions.iter().for_each(|a| {
+        auctions_ref.push((a, a.start_timestamp));
+        auctions_ref.push((a, a.end_timestamp));
+    });
+    auctions_ref.sort_by(|a, b| a.1.cmp(&b.1));
 
-    let mut consumer = channel
-        .basic_consume(
-            "test_queue",
-            "my_consumer",
-            BasicConsumeOptions::default(),
-            FieldTable::default(),
-        )
-        .await?;
 
-    println!("Waiting for messages...");
+    for (auction, timestamp) in auctions_ref {
+        let mut now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+        if timestamp > now {
+            tokio::time::sleep(tokio::time::Duration::from_millis((timestamp - now) as u64)).await;
+        }
 
-    while let Some(delivery) = consumer.next().await {
-        let delivery = delivery?;
-        println!("Received: {:?}", std::str::from_utf8(&delivery.data)?);
-        delivery.ack(Default::default()).await?;
+        now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+        let queue = if now > auction.end_timestamp { 
+            "leilao_finalizado" 
+        } else { 
+            "leilao_iniciado" 
+        };
+
+        channel.basic_publish(
+            "", 
+            queue, 
+            lapin::options::BasicPublishOptions::default(), 
+            auction.id.to_ne_bytes().as_ref(), 
+            lapin::BasicProperties::default()
+        ).await?.await?;
+        println!("Published to {}: {}", queue, auction.id);
     }
 
     Ok(())
+}
+
+
+fn get_auctions() -> Vec<Auction> {
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+    vec![
+        Auction::new(
+            1, 
+            "1L de água de poça".to_string(), 
+            now,
+            now + 15 * 60 * 1000,        
+        ),
+        Auction::new(
+            2, 
+            "bituca de cigarro".to_string(), 
+            now + 60 * 1000,
+            now + 20 * 60 * 1000,
+        ),
+    ]
 }
