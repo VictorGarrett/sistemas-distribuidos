b47fe0ef10cbc3209ca2964c2608430ca3c80c43
hzambenedetti <hique.rz03@gmail.com>
2025-09-03 14:53:03 -0300

    fix: fixed exchange name on queue bind

---
+++ atividade-01/auction_system/bid-srv/src/constants.rs
@@ -0,0 +1,6 @@
+
+const LEILAO_FINALIZADO: &str = "leilao_finalizado";
+const LEILAO_VENCEDOR: &str = "leilao_vencedor";
+const LEILAO_INICIADO: &str = "leilao_iniciado";
+const LANCE_REALIZADO: &str = "lance_realizado";
+
