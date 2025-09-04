6a319ca4bac7e7625df3566ee4f21714ceec3a9d
hzambenedetti <hique.rz03@gmail.com>
2025-09-03 21:08:15 -0300

    partial: Implemented notification service rabbitmq structures initialization

--- atividade-01/auction_system/notification-srv/src/main.rs
+++ atividade-01/auction_system/notification-srv/src/main.rs
@@ -1,9 +1,9 @@
 use lapin::{
-    options::{BasicConsumeOptions, QueueDeclareOptions},
-    types::FieldTable,
-    Connection, ConnectionProperties,
+    options::{BasicConsumeOptions, ExchangeDeclareOptions, QueueBindOptions, QueueDeclareOptions}, types::FieldTable, Channel, Connect, Connection, ConnectionProperties
 };
 use futures_lite::stream::StreamExt;
+
+use std::{error::Error, sync::Arc};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,32 +11,64 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conn = Connection::connect(addr, ConnectionProperties::default()).await?;
     println!("Consumer connected to RabbitMQ!");
 
+    
+
+    Ok(())
+}
+
+async fn init_rabbitmq_structs(conn: Arc<Connection>) -> Result<(), Box<dyn Error>>{
     let channel = conn.create_channel().await?;
+    init_lance_validado(&channel).await?;
+    init_leilao_vencedor(&channel).await?;
+    init_notificacoes(&channel).await?;
 
-    channel
-        .queue_declare(
-            "test_queue",
-            QueueDeclareOptions::default(),
-            FieldTable::default(),
-        )
-        .await?;
 
-    let mut consumer = channel
-        .basic_consume(
-            "test_queue",
-            "my_consumer",
-            BasicConsumeOptions::default(),
-            FieldTable::default(),
-        )
-        .await?;
+    Ok(())
+}
 
-    println!("Waiting for messages...");
+async fn init_lance_validado(channel: &Channel) -> Result<(), Box<dyn Error>>{
+    channel.queue_declare(
+        "lance_validado", 
+        QueueDeclareOptions::default(), 
+        FieldTable::default()
+    ).await?;
 
-    while let Some(delivery) = consumer.next().await {
-        let delivery = delivery?;
-        println!("Received: {:?}", std::str::from_utf8(&delivery.data)?);
-        delivery.ack(Default::default()).await?;
-    }
+    channel.queue_bind(
+        "lance_validado", 
+        "", 
+        "", 
+        QueueBindOptions::default(), 
+        FieldTable::default()
+    ).await?;
 
+    Ok(())
+}
+
+async fn init_leilao_vencedor(channel: &Channel) -> Result<(), Box<dyn Error>>{
+    channel.queue_declare(
+        "leilao_vencedor", 
+        QueueDeclareOptions::default(), 
+        FieldTable::default()
+    ).await?;
+
+    channel.queue_bind(
+        "leilao_vencedor", 
+        "", 
+        "", 
+        QueueBindOptions::default(), 
+        FieldTable::default()
+    ).await?;
+    Ok(())
+}
+
+async fn init_notificacoes(channel: &Channel) -> Result<(), Box<dyn Error>>{
+    channel.exchange_declare(
+        "notificacoes", 
+        lapin::ExchangeKind::Topic,
+        ExchangeDeclareOptions::default(),
+        FieldTable::default()
+    ).await?;
+
+    
     Ok(())
 }
