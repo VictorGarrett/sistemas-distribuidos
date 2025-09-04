824bd8a121a4c06f2ebbacff3c6dbdddca55eeaa
hzambenedetti <hique.rz03@gmail.com>
2025-09-04 15:24:57 -0300

    log: Added debugging logs to bid-srv

--- atividade-01/auction_system/bid-srv/src/models.rs
+++ atividade-01/auction_system/bid-srv/src/models.rs
@@ -1,7 +1,7 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Bid {
     pub auction_id: u32,
     pub client_id: u32,
