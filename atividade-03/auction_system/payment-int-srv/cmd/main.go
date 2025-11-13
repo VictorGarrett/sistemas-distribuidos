3d55d495bfb94a966954046cb3504479b004b249
hzambenedetti <hique.rz03@gmail.com>
2025-11-13 00:52:37 -0300

    added correct rabbitmq link

--- atividade-03/auction_system/payment-int-srv/cmd/main.go
+++ atividade-03/auction_system/payment-int-srv/cmd/main.go
@@ -29,7 +29,7 @@ func main() {
 	updatesChannel := make(chan models.PaymentUpdate)
 	linksChannel := make(chan models.PaymentLink)
 
-	conn, err := amqp.Dial("rabbitmq-link")
+	conn, err := amqp.Dial("amqp://guest:guest@127.0.0.1:5672/%2f")
 	if err != nil {
 		log.Fatalf("Failed to connect to rabbitmq: %v", err)
 	}
